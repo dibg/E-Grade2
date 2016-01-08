789159c3fcc78000b5fd4aac69b636370c77325c
DimitrisBogas <bj2006s@hotmail.com>
2016-01-08 18:21:28 +0200

    database minor changes

--- login.php
+++ login.php
@@ -1,1 +1,8 @@
+<form action="loginSubmit.php" method="post">
+    <h1> E-Grade Log-in</h1><br>
+    Username:<input type="text" name="username" id="username"  placeholder="Username"><br>
+    Password:<input type="password" name="password" id="password" placeholder="Password"><br>
+    <input type="submit"  name="login" class="login login-submit" value="Sign In">
+</form>
+
 <?php
